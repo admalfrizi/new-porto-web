6b3579ddc3587d295a7b03527ebf9bf21ff3dde0
adamalfarizi <adam.alfarizi.2002@gmail.com>
2025-11-09 22:28:44 +0700

    dwadwadawd

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -49,6 +49,27 @@ const aboutmePage = () => {
 
     useGSAP(() => {
         gsap.registerPlugin(ScrollTrigger, SplitText)
+
+        const mm = gsap.matchMedia();
+
+        mm.add(
+            {
+                // set up any number of arbitrarily-named conditions. The function below will be called when ANY of them match.
+                isDesktop: `(min-width: 1024px)`,
+                isTablet: `(max-width: 1024px)`,
+                isMobile: `(max-width: 500px)`,
+                reduceMotion: "(prefers-reduced-motion: reduce)",
+            },
+            (context) => {
+                // context.conditions has a boolean property for each condition defined above indicating if it's matched or not.
+                let ctxCheck = context.conditions;
+
+                gsap.to(".box", {
+                    rotation: ctxCheck.isDesktop ? 360 : 180, // spin further if desktop
+                    duration: reduceMotion ? 0 : 2, // skip to the end if prefers-reduced-motion
+                });
+            }
+        );
         
         const topSection = gsap.timeline({
             scrollTrigger: {
@@ -103,6 +124,16 @@ const aboutmePage = () => {
         const skillsTechSection = gsap.timeline({
             scrollTrigger: {
                 trigger: ".skillsTechSection", 
+                scroller: mainContainer.current,
+                start: 'top 90%', 
+                end: 'bottom 6%',
+                toggleActions: "restart none restart none",
+            }
+        })
+
+        const experienceWorks = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".experienceSection", 
                 scroller: mainContainer.current,
                 start: 'top 90%', 
                 end: 'bottom 6%',
@@ -204,6 +235,37 @@ const aboutmePage = () => {
                 ease: 'power3.out',
                 stagger: 0.2,
             })
+        
+        experienceWorks.from(".title-xp", 
+                {
+                    opacity: 0,
+                    y: -60,
+                    duration: 1.4,
+                    delay: 0.2,
+                    ease: 'power3.out',
+                    stagger: 0.2,
+                },0
+            )
+            .from(".line-animated",
+                {
+                    scaleX: 0,
+                    duration: 1.5,
+                    ease: 'power3.out',
+                    transformOrigin: 'left center',
+                },0
+            ) .from(".dot",{
+                scale: 0,
+                duration: 1,
+                transformOrigin: "center center",
+                ease: "power2.inOut"
+            },0).from(".line-timeline",{
+                scaleX: 0,
+                duration: 1.5,
+                ease: 'power3.out',
+                delay: 1,
+                transformOrigin: 'left center',
+            },0)
+           
     })
 
     return (
@@ -389,20 +451,20 @@ const aboutmePage = () => {
                     
                 </div>
             </div>
-            <div className='h-130 px-5 md:px-24 py-8 flex flex-col items-center bg-cover bg-brand-50 bg-center'>
+            <div className='experienceSection h-130 max-[480px]:h-fit px-5 md:px-24 py-8 max-[480px]:py-0 max-[480px]:pt-8 flex flex-col items-center bg-cover bg-brand-50 bg-center'>
                 <div className='flex flex-col w-full'>
-                    <h1 className='mb-3 font-semibold text-4xl max-[380px]:text-3xl text-brand-900'>Work Experience</h1>
+                    <h1 className='title-xp mb-3 font-semibold text-4xl max-[380px]:text-3xl text-brand-900'>Work Experience</h1>
                     <div className="w-69 max-[380px]:w-58 h-1 bg-brand-900 rounded-full"/>
                 </div>
-                <div className='w-full overflow-x-auto scrollbar-hide py-5 h-full'>
+                <div className='w-full overflow-x-auto max-[480px]:overflow-y-auto scrollbar-hide py-5 h-full'>
                     <div className="relative inline-flex flex-nowrap min-w-full py-4">
-                        <div className="absolute top-4/4 left-0 w-full h-2 bg-brand-900 rounded-full z-0"></div>
-                        <div className="relative flex justify-between w-fit z-10">
+                        <div className="line-timeline absolute top-4/4 max-[480px]:top-4 max-[480px]:left-2.5 left-0 w-full h-2 max-[480px]:w-2 max-[480px]:h-6/8 bg-brand-900 rounded-full z-0"></div>
+                        <div className="relative flex max-[480px]:flex-col justify-between w-fit max-[480px]:w-screen max-[480px]:h-fit z-10">
                             {
                                 experiences.map((exp, index) => (
-                                    <div key={index} className="relative top-8.5 w-120 flex flex-col items-left">
-                                        <div className="w-7 h-7 bg-inside-dot dark:bg-blue-500 rounded-full border-5 border-brand-900"></div>
-                                        <div className={`absolute top-10 ${lastIdx === index ? 'w-8/8' : 'w-7/8'} cursor-pointer text-left text-brand-900 p-5 bg-card-experience rounded-lg shadow-lg`}>
+                                    <div key={index} className="relative top-8.5 max-[480px]:top-0 w-120 max-[480px]:h-80 flex flex-col items-left">
+                                        <div className="dot w-7 h-7 bg-inside-dot dark:bg-blue-500 rounded-full border-5 border-brand-900"></div>
+                                        <div className={`absolute top-10 max-[480px]:top-0 max-[480px]:left-12 ${lastIdx === index ? 'max-[480px]:h-fit min-[480px]:w-8/8' : 'max-[480px]:h-fit min-[480px]:w-7/8'} cursor-pointer text-left text-brand-900 p-5 bg-card-experience rounded-lg shadow-lg`}>
                                             <div className='flex flex-row justify-between content-center'>
                                                 <span className="text-lg font-bold ">{exp.company}</span>
                                                 <p className='text-[14px] content-center'>{exp.period}</p>
@@ -412,13 +474,16 @@ const aboutmePage = () => {
                                                 <p className='text-[14px]'>|</p>
                                                 <p className='text-[14px]'>{exp.location}</p>
                                             </div>
-                                            <div className='max-w-md text-md font-light line-clamp-4'>
-                                                {exp.description}
+                                            <div className='max-w-md  line-clamp-4'>
+                                                <p className='max-[480px]:text-[14px] text-md font-light'>
+                                                    {exp.description}
+                                                </p>
+                                                
                                             </div>
-                                            <div className='flex flex-row space-x-3 overflow-x-scroll scrollbar-hide w-full pt-5'>
+                                            <div className='flex flex-row max-[480px]:flex-wrap max-[480px]:gap-y-3 space-x-3 overflow-x-scroll scrollbar-hide w-full pt-5'>
                                                 {
                                                     exp.technologies.map((tech, techIndex) => (
-                                                        <span key={techIndex} className='bg-span-experience content-center py-1.5 w-fit px-3 text-[12px] rounded-lg text-white'>
+                                                        <span key={techIndex} className='bg-span-experience content-center py-1.5 w-fit px-3 max-[480px]:text-[10px] text-[12px] rounded-lg text-white'>
                                                             {tech}
                                                         </span>
                                                     ))
