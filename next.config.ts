cffe866591a693ab72ffdbc466b6adfcad5869ae
adamalfarizi <adam.alfarizi.2002@gmail.com>
2025-11-28 15:53:29 +0700

    add basePath for directory

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
+const repoName = "new-porto-web";
+
 const nextConfig: NextConfig = {
   /* config options here */
   cacheComponents: false,
   output: 'export',
+  basePath: isProd ? `/${repoName}` : "",
   images: {
     unoptimized: true,
   },
